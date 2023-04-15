2226d724fe36b14a934f3b19aa4ad8472e63fc8e
Tereza <tereza.chodurova@gmail.com>
2023-04-15 04:39:57 +0200

    Initial Commit

---
+++ NFCtron task/NFCtron_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFCtron_taskApp.swift
+//  NFCtron task
+//
+//  Created by Terka on 15.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct NFCtron_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
